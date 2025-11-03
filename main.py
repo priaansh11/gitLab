c0dd89a671d500633fb59b4caa60d4ba9a52f64b
Priyanshu Kumar <priyanshukumarofficial11@gmail.com>
2025-11-03 14:32:18 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def gradient_descent(x,y):
+    m_cur=b_cur=0
+    iter=1000
+    n=len(x)
+    Lr=0.08
+    for i in range(iter):
+        yp = m_cur*x+b_cur
+        cost=(1/n)*sum([val**2 for val in (y-yp)])
+        md=-(2/n)*sum(x*(y-yp))
+        bd=-(2/n)*sum(y-yp)
+        m_cur=m_cur-Lr * md
+        b_cur=b_cur-Lr*bd
+        print("m {}, b {}, cost{} iteration {}".format(m_cur,b_cur,cost,i))
+    pass    
+x=np.array([1,2,3,4,5])
+y=np.array([5,7,9,11,13])
+
+gradient_descent(x,y)
